d3834020707194c9660c904ed62a3c0a63e05e10
Artur Sosnowski <asosnowski@clifford-thames.com>
2016-12-22 11:43:31 +0100

    creating hero object by myself in test purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,19 @@ import { Component } from '@angular/core';
 export class Hero{
   id: number;
   name: string;
+  realName: string;
+  realSurname: string;
+
+  constructor(id: number, name: string, realName: string, realSurname: string){
+    this.id = id;
+    this.name = name;
+    this.realName = realName;
+    this.realSurname = realSurname;
+  }
+
+  get FullRealName():string{
+    return `${this.realName} ${this.realSurname}`;
+  }
 }
 
 
@@ -13,8 +26,9 @@ export class Hero{
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  hero: Hero =  new Hero(1, 'Windstorm', 'testReaLName','testRealSurname');
+  // = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 }
